701e02704ca9b04c70dbf4358895beb38e947a03
Sushil607 <bodadesushil@gmail.com>
2023-02-06 09:34:47 +0530

    create sign in, sign up and forgot password pages and oauth component

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,49 @@
-export default function SignUp(){
-    return(
-        <div>Sign Up</div>
-    )
+import {useState} from 'react'
+import OAuth from '../components/OAuth';
+import { Link } from 'react-router-dom';
+import {AiFillEyeInvisible,AiFillEye} from "react-icons/ai";
+export default function SignUp() {
+  const [showPassword,setShowPassword] = useState(false);
+  const [formData,setFormData] = useState({
+    name:"",
+    email:"",
+    password:""
+  })
+  const {name,email,password} = formData;
+  function onChange(e){
+    setFormData((prevState)=> ({
+     ...prevState,
+     [e.target.id]:e.target.value,
+    }))
+  }
+  return (
+    <section>
+      <h1 className='text-3xl text-center mt-6 font-bold '>Sign Up</h1>
+      <div className='flex justify-center items-center flex-wrap px-6 py-12 max-w-6xl mx-auto'>
+        <div className='md:w-[67%] lg:w-[50%] mb-12 md:mb-6'>
+          <img className='w-full rounded-xl'  src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=773&q=80" alt="key" />
+        </div>
+        <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
+          <form>
+             <input className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white
+            border-gray-300 rounded transition ease-in-out' type="text" id='name' value={name} onChange={onChange} placeholder="Full Name"/>
+            <input className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white
+            border-gray-300 rounded transition ease-in-out' type="email" id='email' value={email} onChange={onChange} placeholder="Email Address"/>
+            <div className='relative mb-6'>
+              <input className='w-full px-4 py-2 text-xl text-gray-700 bg-white
+               border-gray-300 rounded transition ease-in-out' type={showPassword ? "text" : "password"} id='password' value={password} onChange={onChange} placeholder="Password"/>
+               {showPassword ? <AiFillEyeInvisible className='absolute right-3 top-3 text-xl cursor-pointer' onClick={()=> setShowPassword((prevState)=> !prevState)}/> : <AiFillEye className='absolute right-3 top-3 text-xl cursor-pointer' onClick={()=> setShowPassword((prevState)=> !prevState) }/>}
+            </div>
+            <div className='mb-6 flex justify-between whitespace-nowrap text-sm sm:text-lg'>
+              <p>Have an account? <Link to="/sign-in" className='text-red-600 hover:text-red-700 transition duration-200 ease-in-out'>Sign In</Link> </p>
+              <p><Link to="/forgot-password" className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out'>Forgot Password?</Link> </p>
+            </div>
+            <button type="submit" className='w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800'>Sign Up</button>
+            <div className='my-4 flex items-center before:border-t before:border-gray-300 before:flex-1 after:border-t after:border-gray-300 after:flex-1 '><p className='text-center font-semibold mx-4'>OR</p></div>
+            <OAuth/>
+          </form>
+        </div>
+      </div>
+    </section>
+  )
 }
